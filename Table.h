78b004b5e1e2b0e5e96b5972da9a06d2c6978ca2
abbottmb12 <abbottmb12@gmail.com>
2013-09-03 21:36:59 -0500

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,15 +1,22 @@
+#ifndef TABLE_H
+#define TABLE_H
 #include <iostream>
 #include <vector>
 using namespace std;
 //--------------------------------------------------------------------
 //Table class for reading in data
 //Input data format:
+//First: # of cars Then: # of attributes
 //Header: -Attribute Title- -Attribute Title- -Attribute Title- ...etc
 //Tuples: -Attribute-       -Attribute-       -Attribute-       ...etc
 //--------------------------------------------------------------------
 class Table{
-	private: 
+	private:
 		vector< vector<string> > attr; //attributes vectors
-		vector< vector<string>* > tuples; //vector pointing to the tuples in the attr vector not sure about this yet but we need something like this to keep track of tuples
+		vector< vector<string> > tuples; //vector pointing to the tuples in the attr vector
 	public: 
-}
+		Table() : attr(), tuples() {}//default constructor
+		void read(string s);//reading in the data with string s as the filename
+		void dis_table(); //display the table
+};
+#endif
