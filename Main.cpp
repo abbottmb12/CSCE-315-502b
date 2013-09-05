d0032b644e2a1c6d4e44e96085d8e5364c4240e9
abbottmb12 <abbottmb12@gmail.com>
2013-09-04 19:07:53 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include "Table.h"
-//using namespace std;
-//Main Test File
+#include "Relation.h"
+using namespace std;
 int main(){
-	Table T;
-	string file = "Cars.txt";
-	T.read(file);
-	T.dis_table();
+	//Table T;
+	//string file = "Cars.txt";
+	//T.read(file);
+	//T.dis_table();
+	Attribute<int> a;
+	Attribute<int> b;
+	a.cell.push_back(2);
+	b.cell.push_back(3);
+	Relation r;
+	r.add_attr((void*)&b);
+	Attribute<int>* attr = static_cast<Attribute<int>*>(r.attr[0]);
+	//cout << attr->getCells()[0] << endl;
+	cout << (&a)->cell[0] << endl;
+	cout << &b << endl;
 	return 0;
 }
