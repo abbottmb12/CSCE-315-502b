e86f60ec1cf9f1accd79c85e3a061ff1dcf11dfe
abbottmb12 <abbottmb12@gmail.com>
2013-09-06 17:31:38 -0500

    Update Attribute.h

--- Attribute.h
+++ Attribute.h
@@ -2,21 +2,49 @@
 #define ATTRIBUTE_H
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-template <class T>
+enum Type {INT, STRING};
+union Value {
+  int i;
+  char* c;
+};
+struct Cell {
+  Type type;
+  Value value;
+};
+
 class Attribute{
-	private:
-		string name;
-	public:
-		vector<T> cell;
-		Attribute() {};
-		vector<T> getCells(){
-			return cell;
-		}
-		string test() {
-			return "jlfkds";
-		}
-		//string getName();
+  public:
+    string name;
+  public:
+    Type type;
+	vector<Cell> cell;
+    Attribute () {};
+    Attribute(Type t, string n) : type(t), name(n) {};
+   
+    void push_back(Value& v)
+    {
+      Cell c;
+      c.type = type;
+      c.value = v;
+      cell.push_back(c);
+    }
+    vector<Cell> getCells(){
+      return cell;
+    }
+    string getName()
+    {
+      return name;
+    }
+	int getLength(){
+		return cell.size();
+	}
+	void setName(string set_name)
+    {
+      name = set_name;
+    }
+    //string getName();
 };
 #endif
