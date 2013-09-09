de6923bc7a3d5c9fa2fd8a964938ac8e1af30db1
abbottmb12 <abbottmb12@gmail.com>
2013-09-08 23:21:18 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,14 +7,56 @@ using namespace std;
 
 int main(){
   Database d;
-  d.Create("Cars", "Make", "Year", "Model");
-  d.Insert("Cars", "Audi", 2013,"R8");
-  d.Insert("Cars", "Chevrolet", 2011,"Cobalt");
-  d.Insert("Cars", "Mercedes",2010,"Benz");
-  d.Insert("Cars", "Dodge",2009,"Charger");
-  d.Insert("Cars", "Nissan", 2006 , "Altima");
+  d.Create("Cars");
+  d.AddColumn("Cars",Header("Make",STRING));
+  d.AddColumn("Cars",Header("Year",INT));
+  d.AddColumn("Cars",Header("ChangeThisName",STRING));
+  vector<Cell> cell;
+  cell.push_back("Audi");
+  cell.push_back("2013");
+  cell.push_back("R8");
+  d.Insert("Cars", cell);
+  cell.clear();
+  cell.push_back("Audi2");
+  cell.push_back("2013");
+  cell.push_back("R8222222");
+  d.Insert("Cars", cell);
+
+  d.Create("LOL");
+  d.AddColumn("LOL",Header("Hello",STRING));
+  d.AddColumn("LOL",Header("Bello",STRING));
+  d.AddColumn("LOL",Header("haha",STRING));
+  
+  vector<Cell> cell1;
+  vector<Cell> cell2;
+  cell1.push_back("Chevy");
+  cell1.push_back("2001");
+  cell1.push_back("Silverado");
+  cell2.push_back("Audi");
+  cell2.push_back("2012");
+  cell2.push_back("R0");
+  d.Insert("LOL",cell2);
+  d.Insert("LOL",cell1);
 
   d.Show("Cars");
-
+  d.Rename("Cars","Model","ChangeThisName");
+  //d.Delete_attr("Cars","Audi");
+  d.Delete_attr("Cars","Joinks");
+  d.Delete_attr("haha","Audi");
+  d.Show("Cars");
+  d.Show("LOL");
+  d.Union("Cars","LOL");
+  //d.AddColumn("CarsLOLUnion",Header("Union",STRING));
+  //d.Show("CarsLOLUnion");
+  d.Show("kjlk");
+  d.Show("Cars");
+  d.Write("Cars");
+  d.Select("Year", "!=", "2012", "Cars");
+  d.Selection.Show();
+  vector<string> attribute_projection_list;
+  attribute_projection_list.push_back("Make");
+  attribute_projection_list.push_back("Model");
+  d.Project(attribute_projection_list, "Cars");
+  d.Projection.Show();
   return 0;
 }
