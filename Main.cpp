a182e6bccfbd6e9590f66f4f1c908c76ba3b4da5
abbottmb12 <abbottmb12@gmail.com>
2013-09-13 17:03:50 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,61 +2,167 @@
 #include "Relation.h"
 #include "Database.h"
 #include "Attribute.h"
+#include "Token.h"
 
 using namespace std;
 
+Token_stream ts;
+
+bool identifier(Token_stream& ts)
+{
+	string identifier_name = "";
+	Token t = ts.get();
+	if(t.kind != 'a')
+	{
+		ts.putback(t);
+		return false;
+	}
+	identifier_name += t.value;
+	t = ts.get();
+	while(t.kind == 'a' || t.kind == '0')
+	{
+		identifier_name += t.value;
+		t = ts.get();
+	}
+	ts.putback(t);
+	cout << identifier_name << endl;
+	return true;
+}
+bool relation_name(Token_stream& ts)
+{
+	return identifier(ts);
+}
+bool expr(Token_stream& ts)
+{
+	return true;
+}
+bool atomic_expr(Token_stream& ts){
+	return relation_name(ts) || expr(ts);
+}
+bool open(Token_stream& ts){
+	string open = "";
+	Token t = ts.get();
+	vector<Token> tt; //keeps track of tokens that are cycle through
+	open += t.value;
+	tt.push_back(t);
+	while(t.kind = 'a' && (open.size() < 4) ){
+		t = ts.get();
+		open += t.value;
+		tt.push_back(t);
+		if(open == "OPEN"){
+			cout << "opened" << endl;
+			return relation_name(ts);
+		}
+	}
+	for(int i = 0; i<tt.size(); ++i){
+		ts.putback(tt[i]);
+	}
+	return false;
+	
+}
+bool command(Token_stream& ts)
+{
+	return open(ts);
+}
+bool query(Token_stream& ts)
+{
+	if(!relation_name(ts))
+		return false;
+	Token next = ts.get();
+	if(next.kind != ':')
+	{
+		ts.putback(next);
+		return false;
+	}
+	if(!expr(ts))
+		return false;
+	return true;
+}
+bool program(Token_stream& ts)
+{
+	return command(ts) || query(ts);
+}
 int main(){
-  Database d;
-  d.Create("Cars");
-  d.AddColumn("Cars",Header("Make",STRING));
-  d.AddColumn("Cars",Header("Year",INT));
-  d.AddColumn("Cars",Header("ChangeThisName",STRING));
+  /*d.Create("Table1");
+  d.AddColumn("Table1", Header("Str1", STRING));
+  d.AddColumn("Table1", Header("Str2", STRING));
+  d.AddColumn("Table1", Header("Str3", STRING));
   vector<Cell> cell;
-  cell.push_back("Audi");
-  cell.push_back("2013");
-  cell.push_back("R8");
-  d.Insert("Cars", cell);
+  cell.push_back("1.1");
+  cell.push_back("1.2");
+  cell.push_back("1.3");
+  d.Insert("Table1", cell);
   cell.clear();
-  cell.push_back("Audi2");
-  cell.push_back("2013");
-  cell.push_back("R8222222");
-  d.Insert("Cars", cell);
-
-  d.Create("LOL");
-  d.AddColumn("LOL",Header("Hello",STRING));
-  d.AddColumn("LOL",Header("Bello",STRING));
-  d.AddColumn("LOL",Header("haha",STRING));
+  cell.push_back("1.4");
+  cell.push_back("1.5");
+  cell.push_back("1.6");
+  d.Insert("Table1", cell);
+  cell.clear();
+  cell.push_back("1.7");
+  cell.push_back("1.8");
+  cell.push_back("1.9");
+  d.Insert("Table1", cell);
   
-  vector<Cell> cell1;
-  vector<Cell> cell2;
-  cell1.push_back("Chevy");
-  cell1.push_back("2001");
-  cell1.push_back("Silverado");
-  cell2.push_back("Audi");
-  cell2.push_back("2012");
-  cell2.push_back("R0");
-  d.Insert("LOL",cell2);
-  d.Insert("LOL",cell1);
-
-  d.Show("Cars");
-  d.Rename("Cars","Model","ChangeThisName");
-  //d.Delete_attr("Cars","Audi");
-  d.Delete_attr("Cars","Joinks");
-  d.Delete_attr("haha","Audi");
-  d.Show("Cars");
-  d.Show("LOL");
-  d.Union("Cars","LOL");
-  //d.AddColumn("CarsLOLUnion",Header("Union",STRING));
-  //d.Show("CarsLOLUnion");
-  d.Show("kjlk");
-  d.Show("Cars");
-  d.Write("Cars");
-  d.Select("Year", "!=", "2012", "Cars");
-  d.Selection.Show();
-  vector<string> attribute_projection_list;
-  attribute_projection_list.push_back("Make");
-  attribute_projection_list.push_back("Model");
-  d.Project(attribute_projection_list, "Cars");
-  d.Projection.Show();
+  d.Create("Table2");
+  d.AddColumn("Table2", Header("Str1", STRING));
+  d.AddColumn("Table2", Header("Str2", STRING));
+  d.AddColumn("Table2", Header("Str3", STRING));
+  cell.clear();
+  cell.push_back("2.1");
+  cell.push_back("2.2");
+  cell.push_back("2.3");
+  d.Insert("Table2", cell);
+  cell.clear();
+  cell.push_back("2.4");
+  cell.push_back("2.5");
+  cell.push_back("2.6");
+  d.Insert("Table2", cell);
+  cell.clear();
+  cell.push_back("2.7");
+  cell.push_back("2.8");
+  cell.push_back("2.9");
+  d.Insert("Table2", cell);
+  
+  d.Create("Table3");
+  d.AddColumn("Table3", Header("Str1", STRING));
+  d.AddColumn("Table3", Header("Str2", STRING));
+  d.AddColumn("Table3", Header("Str3", STRING));
+  cell.clear();
+  cell.push_back("2.1");
+  cell.push_back("2.2");
+  cell.push_back("2.3");
+  d.Insert("Table3", cell);
+  cell.clear();
+  cell.push_back("3.4");
+  cell.push_back("3.5");
+  cell.push_back("3.6");
+  d.Insert("Table3", cell);
+  cell.clear();
+  cell.push_back("2.7");
+  cell.push_back("2.8");
+  cell.push_back("2.9");
+  d.Insert("Table3", cell);
+  
+  d.Show("Table3");
+  d.Update("Table3", "Str2", "6", "Str3", "==", "2.9");
+  d.Show("Table3");*/
+  Token_stream ts;
+  ts.ss.clear();
+  ts.ss.str("");
+  cout << "Input line: ";
+  string line;
+  getline(cin, line);
+  ts.ss << line;
+  bool valid = true;
+  while(true)
+  {
+	Token t = ts.get();
+	//cout << t.kind << ", " << t.value << endl;
+	if(t.kind == ';')
+		//break;
+		return 0;
+	ts.putback(t);
+	cout << program(ts) << endl;
+  }
   return 0;
 }
