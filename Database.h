bf01e320fe98b95c19bd64539d4d8e188a770856
abbottmb12 <abbottmb12@gmail.com>
2013-09-30 17:11:54 -0500

    Update Database.h

--- Database.h
+++ Database.h
@@ -1,30 +1,39 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
 #include <iostream>
 #include <map>
+#include <exception>
 #include "Relation.h"
 
 using namespace std;
 
-class Database {
-  public:
-	typedef map<string,Relation> Table;
-	vector<Relation> relation;
-	Database(){Create("Selection"); Create("Projection"); };
-	//Database(){};
-	Table table;
-	void Select(string attr_name, string condition, string cell_condition, string rel_name);
-	void Update(string rel_name, string attr_name, string literal, string condition_attr, string condition, string condition_literal);
-	void Delete(string rel_name, string condition_attr, string condition, string condition_literal);
-	void Project(vector<string> attr_name, string rel_name);
-	void Create(string rel_name);
-	//void Delete(string rel_name);
-	void AddColumn(const string& rel_name, const Header& h);
-	void Insert(string rel_name, const vector<Cell>& row);
-	void Write(const string& rel_name);
-	void Show(string rel_name);
-	void Rename(string rel_name,string new_name, string old_name);
-	Relation& operator[](const string& s);
-	void Delete_attr(const string& rel_name,const string& attribute);
-	void Union(const string& rel_name1,const string& rel_name2, const string& rel_name3);
-	void Difference(const string& rel_name1, const string& rel_name2, const string& rel_name3);
-	void Product(const string& rel_name1, const string& rel_name2, const string& rel_name3);
+class Database
+{
+private:
+	vector<Relation*> relations;
+public:
+	int size() { return relations.size(); }
+	bool has_relation(string name);
+	void push_back(Relation* r) { relations.push_back(r); }
+	void set_relation(string name, Relation* r);
+	void write(string name);
+	void close();
+	void open(string name);
+	Relation* get_relation(int i) { return relations[i]; }
+	Relation* get_relation(string name);
+	Relation* create(string name, vector<Attribute> attributes, vector<string> primary);
+	Relation* insert(string name, vector<string> literal_list);
+	Relation* insert(string name, Relation* expr);
+	Relation* select_literal(string attribute_name, string op, string literal, Relation* expr);
+	Relation* select_attribute(string attribute_name1, string op, string attribute_name2, Relation* expr);
+	Relation* project(vector<string> attribute_list, Relation* atomic_expr);
+	Relation* update(vector<string> attribute_list, vector<Cell> cells, Relation* atomic_expr);
+	Relation* rename(vector<string> attribute_list, Relation* atomic_expr);
+	Relation* _union(Relation* r1, Relation* r2);
+	Relation* intersection(Relation* r1, Relation* r2);
+	Relation* difference(Relation* r1, Relation* r2);
+	Relation* product(Relation* r1, Relation* r2);
 };
+
+#endif
